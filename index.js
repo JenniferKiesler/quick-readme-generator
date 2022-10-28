fc0d9c6aec5c9381ff5d8c8e6554d32d45cdc8e2
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-10-27 22:21:07 -0500

    adds function to write readme file

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err
+        console.log('README saved!')
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
