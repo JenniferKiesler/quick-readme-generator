10d0290d546d4e843d1203c84275819a9087a3a9
JKies <jennyhawes24@gmail.com>
2022-11-29 22:58:43 -0600

    deletes TODO comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -54,7 +52,6 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if (err) throw err
@@ -62,7 +59,6 @@ function writeToFile(fileName, data) {
     })
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then((answers) => {
